021976df32edf7820f4a84a4075772e61a8b1f06
dakbill <dakbill@yahoo.com>
2012-06-20 14:41:02 +0000

    Lab 2

---
+++ dak.py
@@ -0,0 +1,78 @@
+x = 2 ; y = 5
+if x>2:
+    if y>2:
+        z=x+y
+        print "z is ",z
+else:
+	print "x is ",x
+
+x = 3 ; y = 1
+if x>2:
+    if y>2:
+        z=x+y
+        print "z is ",z
+else:
+	print "x is ",x
+
+x = 1 ; y = 1
+if x>2:
+    if y>2:
+        z=x+y
+        print "z is ",z
+else:
+	print "x is ",x
+
+x = 4 ; y = 3
+
+if x>2:
+    if y>2:
+        z=x+y
+        print "z is ",z
+else:
+	print "x is ",x
+##############################
+x=3;y=5
+z = x+3
+if z==1:
+    y=0
+    print y
+elif z==2:
+    print y
+    y=10
+elif z==4:
+    print y
+    y+=1
+else:
+    y=1
+    print y
+##############################
+    print
+x=2;y=5
+z = x+3
+if z==1:
+    y=0
+    print y
+elif z==2:
+    print y
+    y=10
+elif z==4:
+    print y
+    y+=1
+else:
+    y=1
+    print y
+#############################
+    print
+    i=0
+    while i<10:
+        i+=1
+        if i%2==0:
+            print i
+#############################
+print
+i=0
+while i > 10:
+    i+=1
+    if i%2 == 0:
+       print i
+
