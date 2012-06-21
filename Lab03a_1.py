6cc3acb91d99da6cc391fc829642d6160c972ee7
Kofi Dwomoh-Ababio <dakbill@DAK.(none)>
2012-06-21 09:27:50 +0000

    reverse num

--- Lab03a_1.py
+++ Lab03a_1.py
@@ -3,7 +3,7 @@ number=float(raw_input("Enter the number:"))
 temp=0
 tnum=number
 A=""
-for i in range(0,int(math.log(number)+2),1):
+for i in range(int((math.log10(number)+1)-1),-1,-1):
         temp+=(tnum%10)*(10**i)
         tnum=int(tnum/10)
-print temp
+print int(temp)
